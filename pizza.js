f029ca33bf293871507f930606f83be27515f6a8
Nkateko00 <tekonku@icloud.com>
2020-06-10 10:03:43 +0200

    linking

--- pizza.js
+++ pizza.js
@@ -10,20 +10,33 @@ const grandTotalElem = document.querySelector('.grandTotal');
 const smallTotalPrice =  document.querySelector(".smallTotalPrice");
 const mediumTotalPrice = document.querySelector(".mediumTotalPrice");
 const largeTotalPrice =  document.querySelector(".largeTotalPrice");
+const totalPrice =  document.querySelector(".totalPrice");
 
 const smallQty =  document.querySelector(".smallQty");
 const mediumQty = document.querySelector(".mediumQty");
 const largeQty =  document.querySelector(".largeQty");
 
+const orderPizza = OrderPizza();
 
 orderSmallPizza.addEventListener('click', function(e){
-	alert("order small pizza : " + smallQty.value);
+
+	orderPizza.orderSmall( smallQty.value);
+	smallTotalPrice.innerHTML = orderPizza.smallTotal();
+	totalPrice.innerHTML = orderPizza.total();
+ 
 });
 
 orderMediumPizza.addEventListener('click', function(e){
-	alert("order medium pizza : " + mediumQty.value);
+	orderPizza.orderMedium(mediumQty.value);
+	mediumTotalPrice.innerHTML = orderPizza.mediumTotal();
+	totalPrice.innerHTML = orderPizza.total();
 });
 
 orderLargePizza.addEventListener('click', function(e){
-	alert("order large pizza : " + largeQty.value);
+	orderPizza.orderLarge(largeQty.value);
+	//get the value  inserted
+	largeTotalPrice.innerHTML = orderPizza.largeTotal();
+	//adding to the value received to the large total method
+	totalPrice.innerHTML = orderPizza.total();
+	//adding to the value received to the overall total method
 });
